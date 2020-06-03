d8145d524c4cfd74c19f4537c7687558d66026ee
yossaj <yossaj@icloud.com>
2020-06-03 13:19:59 +0100

    recycler displaying fake data

--- app/src/main/java/com/example/vendingmachine/tasks/TasksAdapter.kt
+++ app/src/main/java/com/example/vendingmachine/tasks/TasksAdapter.kt
@@ -2,18 +2,42 @@ package com.example.vendingmachine.tasks
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.vendingmachine.data.Task
+import com.example.vendingmachine.databinding.TaskItemBinding
 
-class TasksAdapter : ListAdapter<Task, TasksAdapter.ViewHolder>(TaskDiffCallback()){
+class TasksAdapter : ListAdapter<Task, TasksAdapter.TaskViewHolder>(TaskDiffCallback()){
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = TaskItemBinding.inflate(layoutInflater, parent, false)
+        return TaskViewHolder(binding)
 
+    }
 
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+        val task = getItem(position)
+        holder.bind(task)
+    }
 
+    class TaskViewHolder(val binding: TaskItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
 
-    class ViewHolder private constructor() :
-        RecyclerView.ViewHolder() {
+        fun bind(task: Task){
+            binding.taskItemTitle.text = task.title
+        }
 
+    }
+
+    class TaskDiffCallback() : DiffUtil.ItemCallback<Task>(){
+        override fun areItemsTheSame(oldItem: Task, newItem: Task): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Task, newItem: Task): Boolean {
+            return oldItem.equals(newItem)
+        }
     }
 }
--- app/src/main/java/com/example/vendingmachine/tasks/TasksFragment.kt
+++ app/src/main/java/com/example/vendingmachine/tasks/TasksFragment.kt
@@ -5,6 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import com.example.vendingmachine.data.Task
 import com.example.vendingmachine.databinding.FragmentTasksBinding
 
 class TasksFragment : Fragment(){
@@ -14,7 +18,23 @@ class TasksFragment : Fragment(){
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        fakeDataList()
         val binding = FragmentTasksBinding.inflate(inflater)
+        val adapter = TasksAdapter()
+        adapter.submitList(fakeDataList())
+        binding.taskHabitList.adapter = adapter
         return binding.root
+    }
+
+
+    fun fakeDataList() : MutableList<Task>{
+        val list = mutableListOf<Task>()
+        val task1 = Task("New Task", "Some stuff")
+        val task2 = Task("Some Task", "Some stuff")
+        val task3 = Task("Some Other Task", "Some stuff")
+        list.add(task1)
+        list.add(task2)
+        list.add(task3)
+        return list
     }
 }
