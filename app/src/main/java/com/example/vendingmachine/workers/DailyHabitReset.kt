18dd7e1e7d2ac22163b7558d4933d0308014dec2
yossaj <yossaj@icloud.com>
2020-06-13 22:55:17 +0100

    minor tidy up of habit check worker

--- app/src/main/java/com/example/vendingmachine/workers/DailyHabitReset.kt
+++ app/src/main/java/com/example/vendingmachine/workers/DailyHabitReset.kt
@@ -5,33 +5,34 @@ import android.util.Log
 import android.util.TimeUtils
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import com.example.vendingmachine.R
 import com.example.vendingmachine.data.TaskDatabase
 import kotlinx.coroutines.delay
 import java.util.concurrent.TimeUnit
 
-class DailyHabitReset(context: Context, params : WorkerParameters) : Worker(context, params){
+class DailyHabitReset(context: Context, params: WorkerParameters) : Worker(context, params) {
 
     override fun doWork(): Result {
         val appContext = applicationContext
 
-        return try{
+        return try {
             val datasource = TaskDatabase.getInstance(appContext)
 
             val checkedHabits = datasource.taskDao.getCompleteHabits()
 
             val currentTime = System.currentTimeMillis()
+            val uncheckDely = TimeUnit.HOURS.toMillis(20)
 
-            checkedHabits.forEach {it ->
-                val uncheckDely = TimeUnit.HOURS.toMillis(20)
-                val unchecktime = (it.updatedTime + uncheckDely )
-                if(currentTime >= unchecktime) {
+            checkedHabits.forEach { it ->
+                val unchecktime = (it.updatedTime + uncheckDely)
+                if (currentTime >= unchecktime) {
                     it.isCompleted = false
                     datasource.taskDao.updateTask(it)
                 }
             }
             return Result.success()
-        }catch (throwable : Throwable){
-            Log.e("Habit reset", "Couldn't access database", throwable)
+        } catch (throwable: Throwable) {
+            Log.e(appContext.getString(R.string.habit_worker_error_tag), appContext.getString(R.string.habit_worker_error_message), throwable)
             Result.failure()
         }
     }
