602dccac7b045869ac4ff30996155443723664b7
yossaj <yossaj@icloud.com>
2020-09-23 16:10:10 +0100

    d

--- app/src/main/java/com/example/vendingmachine/ui/habits/HabitViewHolder.kt
+++ app/src/main/java/com/example/vendingmachine/ui/habits/HabitViewHolder.kt
@@ -15,10 +15,10 @@ class HabitViewHolder(val binding: HabitItemBinding) :
     fun bind(
         habit: Habit,
         clickListener: HabitAdapter.OnClickListener
-    ){
+    ) {
         binding.habit = habit
-        binding.incrementCountBtn.setOnClickListener{clickListener.onClick(habit, 1)}
-        binding.decrementCountBtn.setOnClickListener{clickListener.onClick(habit, 2)}
+        binding.incrementCountBtn.setOnClickListener { clickListener.onClick(habit, 1) }
+        binding.decrementCountBtn.setOnClickListener { clickListener.onClick(habit, 2) }
         binding.deleteHabitBtn.setOnClickListener {
             clickListener.onClick(habit, 6)
             toggleEditHabit(habit)
@@ -28,21 +28,18 @@ class HabitViewHolder(val binding: HabitItemBinding) :
         binding.repeatHabitMax.setMinValue(1)
         binding.repeatHabitMax.setMaxValue(10)
 
-
-
-
-            binding.updateHabitBtn.setOnClickListener {
-                val title = binding.editHabitTitle.editableText.toString()
-                val max = binding.repeatHabitMax.value
-                if(title.isNotEmpty() && (title != habit.title || max != habit.max)) {
-                    habit.title = title
-                    habit.max = max
-                    clickListener.onClick(habit, 3)
-                    toggleEditHabit(habit)
-                }else{
-                    clickListener.onClick(habit, 4)
-                }
+        binding.updateHabitBtn.setOnClickListener {
+            val title = binding.editHabitTitle.editableText.toString()
+            val max = binding.repeatHabitMax.value
+            if (title.isNotEmpty() && (title != habit.title || max != habit.max)) {
+                habit.title = title
+                habit.max = max
+                clickListener.onClick(habit, 3)
+                toggleEditHabit(habit)
+            } else {
+                clickListener.onClick(habit, 4)
             }
+        }
 
 
 
@@ -55,12 +52,12 @@ class HabitViewHolder(val binding: HabitItemBinding) :
             clickListener.onClick(habit, 5)
         }
 
-        if(habit.count < habit.max) {
+        if (habit.count < habit.max) {
             binding.habitTally.visibility = View.VISIBLE
             val tally = habit.count.toString() + "/" + habit.max.toShort()
             binding.habitTally.text = tally
             binding.habitTallyAnimation.visibility = View.INVISIBLE
-        }else{
+        } else {
             binding.habitTally.visibility = View.INVISIBLE
             binding.habitTallyAnimation.visibility = View.VISIBLE
             val animation = binding.habitTallyAnimation.drawable as Animatable
@@ -69,7 +66,7 @@ class HabitViewHolder(val binding: HabitItemBinding) :
     }
 
     private fun toggleEditHabit(habit: Habit) {
-        if(!binding.editFormContainer.isVisible) {
+        if (!binding.editFormContainer.isVisible) {
             binding.habitTitle.visibility = View.INVISIBLE
             binding.incrementCountBtn.visibility = View.INVISIBLE
             binding.decrementCountBtn.visibility = View.INVISIBLE
@@ -77,14 +74,14 @@ class HabitViewHolder(val binding: HabitItemBinding) :
             binding.habitTallyAnimation.visibility = View.INVISIBLE
             binding.editFormContainer.visibility = View.VISIBLE
             binding.repeatHabitMax.value = habit.max
-        }else{
+        } else {
             binding.editFormContainer.visibility = View.GONE
             binding.habitTitle.visibility = View.VISIBLE
             binding.incrementCountBtn.visibility = View.VISIBLE
             binding.decrementCountBtn.visibility = View.VISIBLE
-            if(habit.count < habit.max){
+            if (habit.count < habit.max) {
                 binding.habitTally.visibility = View.VISIBLE
-            }else{
+            } else {
                 binding.habitTallyAnimation.visibility = View.VISIBLE
             }
         }
